d717b37935bb5cb98a7046bbaf7c097da2fa0f56
st <st@Alaynas-MacBook-Pro.local>
2024-05-30 06:45:49 -0700

    sam eval, init lambda

--- .workspace/sam-eval/README.md
+++ .workspace/sam-eval/README.md
@@ -1,3 +1,25 @@
-- aws sam for fast local dev
-- evaluate use of pandas and pyathena over existing flat json files
-- try out the pandas corr() function
+## sam-eval
+
+I figured I might as well come up with a use case for getting familiar with AWS serverless application model, so I decided to try and find some amount of correlation in the dataset I collected a few years back.
+- There are about 20 entries in the dataset, stored as flat json in S3
+- the `spot_` keys represent locations on the basketball court, and the value for each is the amount made out of 4 attempts
+- the `temp` is the recorded temperature at the time, in Farenheit
+- so in this example, 1 shot out of 4 was made from `spot_1` and the temperature outside was 93, yikes
+```json
+    {
+        "spot_1": "1", 
+        "spot_2": "2", 
+        "spot_3": "1", 
+        "spot_4": "0", 
+        "spot_5": "1", 
+        "spot_6": "1", 
+        "spot_7": "1", 
+        "spot_8": "1", 
+        "spot_9": "4", 
+        "spot_10": "2", 
+        "spot_11": "1",
+        "temp": "93",
+        "date": "2021-06-11",
+        "time": "12:9:25"
+    }
+```
--- .workspace/sam-eval/sam-app/hello_world/app.py
+++ .workspace/sam-eval/sam-app/hello_world/app.py
@@ -1,42 +1,78 @@
 import json
+import os
 
-# import requests
+from pyathena import connect
+import pandas as pd
+
+
+def calculate_success_rates():
+    """Calculate the success rates for each spot on the basketball court.
+    """
+    stage_bucket_name = "shooting-insights-athena-results"
+    stage_bucket_region = "us-east-1"
+    source_bucket_name = "shooting-insights-data"
+    source_bucket_region = "us-east-1"
+    source_json_paths = [
+        "/collection/midrange/",
+        "/collection/threepoint/",
+    ]
+
+    for path in source_json_paths:
+        path_name = os.path.basename(os.path.normpath(path))
+
+        # Connect to Athena
+        conn = connect(s3_staging_dir=f's3://{stage_bucket_name}/sam-eval-athena-results/{path_name}/',
+                       region_name=stage_bucket_region)
+
+        # Define the Athena queries
+        create_table_query = f"""
+        CREATE EXTERNAL TABLE IF NOT EXISTS basketball_shots_{path_name} (
+        `spot_1` int,
+        `spot_2` int,
+        `spot_3` int,
+        `spot_4` int,
+        `spot_5` int,
+        `spot_6` int,
+        `spot_7` int,
+        `spot_8` int,
+        `spot_9` int,
+        `spot_10` int,
+        `spot_11` int,
+        `temp` int,
+        `date` string,
+        `time` string
+        )
+        ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+        WITH LOCATION 's3://{source_bucket_name}{path}'
+        TBLPROPERTIES ('has_encrypted_data'='false');
+        """
+
+        calculate_success_rate_query = """
+        SELECT
+        (SUM(spot_1) + SUM(spot_2) + SUM(spot_3) + SUM(spot_4)) / (4 * COUNT(*)) AS left_success_rate,
+        (SUM(spot_5) + SUM(spot_6) + SUM(spot_7)) / (3 * COUNT(*)) AS center_success_rate,
+        (SUM(spot_8) + SUM(spot_9) + SUM(spot_10) + SUM(spot_11)) / (4 * COUNT(*)) AS right_success_rate
+        FROM basketball_shots;
+        """
+
+        # Execute the queries
+        pd.read_sql(create_table_query, conn)
+        success_rates = pd.read_sql(calculate_success_rate_query, conn)
+
+        # Print the success rates
+        print(success_rates)
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """The entrypoint of the lambda invocation.
     """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello darkness my old friend",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    try:
+        # Connect to Athena and execute the queries
+        calculate_success_rates()
+    except Exception as e:
+        # Log the exception
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps('An error occurred')
+        }
