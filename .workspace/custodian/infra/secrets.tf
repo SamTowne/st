8db90aff8ac7b0fa43791034dfb0d539627081c1
Samuel Towne <chmod777recursively@gmail.com>
2024-06-08 07:19:12 -0700

    custodian backend

---
+++ .workspace/custodian/infra/secrets.tf
@@ -0,0 +1,16 @@
+# docdb
+resource "aws_secretsmanager_secret" "this" {
+  name = "custodian-db"
+  description = "Secret for custodian database"
+}
+
+resource "aws_secretsmanager_secret_version" "this" {
+  secret_id = aws_secretsmanager_secret.this.id
+  secret_string = jsonencode({
+    username = "placeholder_value"
+    password = "placeholder_value"
+  })
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
